9b1caa6472a2a2e16f3986a0ad4e4f905c13eac9
runningIris <irisrunning@foxmail.com>
2016-11-24 15:05:19 +0800

    modification on texts and details

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-let container = document.querySelector('.container');
-const num = prompt('How many boxes do you want?');
-const destiny = Math.floor(Math.random() * num);
-let index = 0;
-for(let i=0; i<num; i++){
-	let box = document.createElement('div');
-	box.className = 'box';
-	container.appendChild(box);
-	box.addEventListener('click', function(){
-		index += 1;
-		if(i == destiny){
-			box.className += ' guessed';
-			alert(`Congratulations, this is the box! It takes you ${index} times to guess it`);
-		} else{
-			box.className += ' wrongGuess'
-			console.log('Sorry, this is not the box.');
-			
-		}
-	});
+function guessBox(){
+	let num = prompt('How many friends do you want?');
+	let destiny = Math.floor(Math.random() * num);
+	let index = 0;
+
+	for(let i=0; i<num; i++){
+		let box = document.createElement('div');
+		box.className = 'box';
+		container.appendChild(box);
+		box.addEventListener('click', function(){
+			index += 1;
+			if(i == destiny){
+				box.className += ' guessed';
+				if(box.className == 'box guessed'){
+					info.innerText = `Congratulations, Nana is here! 
+					It takes you ${index} times to find out Nana.`;
+				}
+				var restart = document.createElement('div');
+				restart.className = 'restart';
+				restart.innerHTML = '<h3>RESTART</h3>';
+				container.appendChild(restart);
+				restart.addEventListener('mouseup', function(){
+					while(container.children.length > 0){
+						container.removeChild(container.children[0])
+					}
+					info.innerText = '';
+					guessBox();
+				})
+			} else{
+				box.className += ' wrongGuess'
+				info.innerText = 'Oops! Nana is not here.';
+
+			}
+		});
+	}
 }
+
+
+let container = document.querySelector('.container');
+let info = document.querySelector('.info');
+guessBox();
